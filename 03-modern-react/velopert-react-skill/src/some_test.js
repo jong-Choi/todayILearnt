bbb32a0ab7cd562de01ffe59c4ac4949ac2035c4
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2022-08-20 23:20:37 +0900

    4주차 완료

---
+++ 03-modern-react/velopert-react-skill/src/some_test.js
@@ -0,0 +1,12 @@
+React.Component.App.render();
+
+class App extends React.Component {
+  render() {
+    const name = this.componentDidCatch;
+    return (
+      <>
+        <li></li>
+      </>
+    );
+  }
+}
