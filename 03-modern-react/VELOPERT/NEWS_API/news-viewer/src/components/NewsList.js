8727262ebbc527bf4ee06371eae8ebb5e2c0c252
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2022-09-22 16:57:07 +0900

    하이하이

--- 03-modern-react/VELOPERT/NEWS_API/news-viewer/src/components/NewsList.js
+++ 03-modern-react/VELOPERT/NEWS_API/news-viewer/src/components/NewsList.js
@@ -1,20 +1,20 @@
-import { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import axios from '../../node_modules/axios/index';
-import NewsItem from './NewsItem';
+// import { useEffect, useState } from 'react';
+// import styled from 'styled-components';
+// import axios from '../../node_modules/axios/index';
+// import NewsItem from './NewsItem';
 
-const NewsItemBlock = styled.div`
-  box-sizing: border-box;
-  padding-bottom: 3rem;
-  width: 768px;
-  margin: 0 auto;
-  margin-top: 2rem;
-  @media screen and (max-width: 768px) {
-    width: 100%;
-    padding-left: 1rem;
-    padding-right: 1rem;
-  }
-`;
+// const NewsItemBlock = styled.div`
+//   box-sizing: border-box;
+//   padding-bottom: 3rem;
+//   width: 768px;
+//   margin: 0 auto;
+//   margin-top: 2rem;
+//   @media screen and (max-width: 768px) {
+//     width: 100%;
+//     padding-left: 1rem;
+//     padding-right: 1rem;
+//   }
+// `;
 
 // const NewsList = ({ category }) => {
 //   const [articles, setArticles] = useState(null);
@@ -45,18 +45,48 @@ const NewsItemBlock = styled.div`
 //     return null;
 //   }
 
-const NewsList = ({category}) => {
-  const query = category === 'all' ? '' : `&category=${category}`;
-  return axios.get(
-              `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=00a04a8d9e9c4429aefb4798d3582e62`,
-            );
-}, [category]);
+import styled from 'styled-components';
+import axios from '../../node_modules/axios/index';
+import NewsItem from './NewsItem';
+import usePromise from '../lib/usePromise';
 
-}
+const NewsItemBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+const NewsList = ({category}) => {
+  const [loading, response, error] = usePromise(()=>{
+    const query = category === 'all'? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=00a04a8d9e9c4429aefb4798d3582e62`,
+    );
+  }, [category]);
+
+  if (loading){
+    return <NewsItemBlock>대기 중...</NewsItemBlock>;
+  }
+
+  if (!response) {
+    return null;
+  }
+
+  if (error){
+    return <NewsItemBlock>에러 발생</NewsItemBlock>
+  }
+
+  const {articles} = response.data;
 
   return (
     <NewsItemBlock>
-      {articles.map((article) => (
+      {articles.map(article => (
         <NewsItem key={article.url} article={article} />
       ))}
     </NewsItemBlock>
