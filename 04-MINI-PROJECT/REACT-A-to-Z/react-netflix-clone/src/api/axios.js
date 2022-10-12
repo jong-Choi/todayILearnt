d1061a177e1d7f8b08385ebf7c6f75b003653602
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2022-10-12 20:06:01 +0900

    슬라이드 기능 추가하지 전까지

--- 04-MINI-PROJECT/REACT-A-to-Z/react-netflix-clone/src/api/axios.js
+++ 04-MINI-PROJECT/REACT-A-to-Z/react-netflix-clone/src/api/axios.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+// 같은 요청을 여러번 반복하기 위해 baseURL, Prams, Fetch(requests.js)를 나누어 저장
+
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: "8506b3753519abc3454b87370bbc859a",
+    language: "ko-KR",
+  },
 });
+
+export default instance;
