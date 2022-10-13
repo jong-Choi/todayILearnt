44271d32dee5119b1ddec2631ce25ed4da34e529
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2022-10-13 17:43:54 +0900

    누이터 215페이지

--- 04-MINI-PROJECT/nwitter/src/routes/Home.js
+++ 04-MINI-PROJECT/nwitter/src/routes/Home.js
@@ -1,14 +1,13 @@
-import { dbService, storageService } from "fbase";
+import { dbService } from "fbase";
 
 import React, { useEffect } from "react";
 import { useState } from "react";
 import Nweet from "components/Nweet";
-import { v4 as uuidv4 } from "uuid";
+
+import NweetFactory from "components/NweetFactory";
 
 function Home({ userObj }) {
-  const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
-  const [attachment, setAttachment] = useState("");
 
   //   프로젝트의 src/index.js에서
   // <React.StrictMode> 태그로 <app/>이 감싸져있으면
@@ -25,72 +24,10 @@ function Home({ userObj }) {
     });
   }, []);
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    let attachmentUrl = "";
-    if (attachment !== "") {
-      const attachmentRef = storageService
-        .ref()
-        .child(`${userObj.uid}/${uuidv4()}`);
-      const response = await attachmentRef.putString(attachment, "data_url");
-      // console.log(await response.ref.getDownloadURL);
-      attachmentUrl = await response.ref.getDownloadURL();
-    }
-    const res = await dbService.collection("nweets").add({
-      text: nweet,
-      createAt: Date.now(),
-      creatorId: userObj.uid,
-      attachmentUrl,
-    });
-    setNweet("");
-    setAttachment("");
-    console.log(res);
-  };
-
-  const onChange = (event) => {
-    event.preventDefault();
-    //아래는 event.target은 이벤트가 발생한 <input> 태그를 의미하며, 해당 태그의 <input value={}> 를 받아온다는 의미임.
-    const {
-      target: { value },
-    } = event;
-    setNweet(value);
-  };
-
-  const onFileChange = (event) => {
-    const {
-      target: { files },
-    } = event;
-    const theFile = files[0];
-    const reader = new FileReader();
-    reader.onloadend = (finishedEvent) => {
-      const {
-        currentTarget: { result },
-      } = finishedEvent;
-      setAttachment(result);
-    };
-    reader.readAsDataURL(theFile);
-  };
-
-  const onClearAttachment = () => setAttachment("");
   return (
     <>
-      <form onSubmit={onSubmit}>
-        <input
-          value={nweet}
-          onChange={onChange}
-          type="text"
-          placeholder="아무 말이나 지껄이세요."
-          maxLength={120}
-        ></input>
-        <input type="file" accept="image/*" onChange={onFileChange} />
-        <input type="submit" value="Nweet" />
-        {attachment && (
-          <div>
-            <img src={attachment} width="50px" height="50px" />
-            <button onClick={onClearAttachment}>Clear</button>
-          </div>
-        )}
-      </form>
+      <NweetFactory userObj={userObj} />
+
       <div>
         {nweets.map((nweet) => (
           <Nweet
