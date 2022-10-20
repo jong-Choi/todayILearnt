51793e5e90d4ce9f36ac955c318275f43b1fa8b5
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2022-10-20 17:45:41 +0900

    211페이지 오류

--- 04-MINI-PROJECT/DRF-Practice/04_MyBoard/posts/serializers.py
+++ 04-MINI-PROJECT/DRF-Practice/04_MyBoard/posts/serializers.py
@@ -1,14 +1,43 @@
 from rest_framework import serializers
+
 from users.serializers import ProfileSerializer
-from .models import Post
+from .models import Post, Comment
+
+
+# 댓글 시리얼라이저를 먼저 선언#
+class CommentSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ("pk", "profile", "post", "text")
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ("post", "text")
+
+
+##############
 
 
 class PostSerializer(serializers.ModelSerializer):
     profile = ProfileSerializer(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
-        fields = ("pk", "profile", "title", "body", "image", "published_date", "likes")
+        fields = (
+            "pk",
+            "profile",
+            "title",
+            "body",
+            "image",
+            "published_date",
+            "likes",
+            "comments",
+        )
 
 
 class PostCreateSerializer(serializers.ModelSerializer):
