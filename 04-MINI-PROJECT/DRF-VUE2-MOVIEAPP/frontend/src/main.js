42a30b051215be42f0db80b9d2d2278575731e50
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2022-11-09 17:59:46 +0900

    우와

--- 04-MINI-PROJECT/DRF-VUE2-MOVIEAPP/frontend/src/main.js
+++ 04-MINI-PROJECT/DRF-VUE2-MOVIEAPP/frontend/src/main.js
@@ -1,15 +1,12 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
 import App from "./App.vue";
-import store from "./store";
 import router from "./router";
-
-Vue.use(VueRouter);
+import store from "./store";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  store,
   router,
+  store,
   render: (h) => h(App),
 }).$mount("#app");
--- 04-MINI-PROJECT/DRF-VUE2-MOVIEAPP/frontend/src/router/index.js
+++ 04-MINI-PROJECT/DRF-VUE2-MOVIEAPP/frontend/src/router/index.js
@@ -1,16 +1,30 @@
 import Vue from "vue";
-import Router from "vue-router";
-import PostListPage from "@/pages/PostListPage";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-export default new Router({
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
+
+const router = new VueRouter({
   mode: "history",
-  routes: [
-    {
-      path: "/",
-      name: "PostListPage",
-      component: PostListPage,
-    },
-  ],
+  base: process.env.BASE_URL,
+  routes,
 });
+
+export default router;
